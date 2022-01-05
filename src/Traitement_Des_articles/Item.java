8b27c21f36cd4207bce374bdd664251ab5830b9a
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-05 16:36:55 +0100

    testing

--- src/Traitement_Des_articles/Item.java
+++ src/Traitement_Des_articles/Item.java
@@ -7,7 +7,7 @@ private final String name;
 private final long price;
 private final int weight;
 //constructor  with arguments
-    public Item(String name, long price, int weight) {
+    public Item(String name, long price,int weight) {
         this.price = price;
         this.name =name;
         this.weight = weight;
@@ -21,7 +21,7 @@ private final int weight;
 //override the println methode to print only objects we want
 @Override
 public String toString() {
-       return name +" : "+price +" MAD : "+weight+" Grammes ";
+       return name +" : "+price +" MAD "+" : "+weight +" Grammes ";
 }
 //getter
 //no setter since we dont wanna change anything
@@ -34,7 +34,7 @@ public String toString() {
     public int getWeight() { return weight;}
 //testing in the main
     public static void main(String[] args) {
-        Item item = new Item("corn flakes", 500, 1000);
+        Item item = new Item("corn flakes", 500,1000);
         System.out.println(item);           // affiche: corn flakes: 5.00 MAD
         Item chewingGum = new Item("chewing gum",403,500);
         System.out.println(chewingGum);    // affiche: chewing gum: 4.03 MAD
--- src/Traitement_des_factures/PanierArticles.java
+++ src/Traitement_des_factures/PanierArticles.java
@@ -11,14 +11,16 @@ public class PanierArticles {
    private static int counter = 0;
    //add to the collection of itens to panierArticles
     public void addItem(Item item) {
-        if(item.getWeight()<=1000){
-            this.items.add(item);
-            counter++;
-        }
+        if (item.getWeight()>1000)
+            throw new IllegalStateException("Weight superior to 10 kgs");
+        else {
+           this.items.add(item);
+           counter++;
+       }
     }
     public boolean removeItem(Item item) {
             if(items.contains(item)) {
-                this.items.remove(item);
+                items.remove(item);
                 return true;
             }
         return false;
@@ -33,4 +35,13 @@ public class PanierArticles {
         }
         return sum;
     }
+    //testing in the main
+        public static void main(String[] args) {
+            PanierArticles panier = new PanierArticles();
+            Item item = new Item("corn flakes", 500,1000);
+            panier.addItem(item);
+            panier.removeItem(new Item("corn flakes", 500,500));
+            System.out.println(panier.itemCount());       // affiche: 0
+
+        }
 }
