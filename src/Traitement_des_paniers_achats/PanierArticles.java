1be63511909f3d17dd0a43540c07f874bfda361f
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-06 00:41:40 +0100

    good ?

--- src/Traitement_des_paniers_achats/PanierArticles.java
+++ src/Traitement_des_paniers_achats/PanierArticles.java
@@ -9,6 +9,11 @@ import java.util.List;
 public class PanierArticles {
     private List<Item> items = new ArrayList<Item>();
     private  int counter = 0;
+    public static int ID;
+    //default constructor
+    public PanierArticles() {
+        ID++;
+    }
     //add to the collection of itens to panierArticles
     public void addItem(Item item) {
         if (item.getWeight()>1000)
@@ -18,6 +23,9 @@ public class PanierArticles {
             counter++;
         }
     }
+    //getter ID
+    public int getID() { return this.ID; }
+    //removeItem
     public boolean removeItem(Item item) {
         if(items.contains(item)) {
             items.remove(item);
@@ -26,9 +34,11 @@ public class PanierArticles {
         }
         return false;
     }
+    //item count
     public int itemCount() {
         return counter;
     }
+    //price total
     public long totalPrice(){
         long sum=0;
         for (Item a : items){
@@ -47,11 +57,19 @@ public class PanierArticles {
     }
     //testing in the main
     public static void main(String[] args) {
-        PanierArticles panier = new PanierArticles();
-        Item item = new Item("corn flakes", 500,1000);
-        panier.addItem(item);
-        System.out.println(panier);
-        panier.removeItem(item);
-        System.out.println(panier.itemCount());       // affiche: 0
+        PanierArticles c1 = new PanierArticles();
+        System.out.println(c1.getID());         // affiche: 1
+        Item item1 = new Item("corn flakes", 501, 1000);
+        c1.addItem(item1);
+        Item item2 = new Item("caviar", 50000, 500);
+        c1.addItem(item2);
+        System.out.println(c1.getID());         // affiche: 1
+        PanierArticles c2 = new PanierArticles();
+        PanierArticles c3 = new PanierArticles();
+        Item item3 = new Item("water", 500, 500);
+        c3.addItem(item3);
+        System.out.println(c2.getID());         // affiche: 2 => it doesnt show 2
+        System.out.println(c3.getID());         // affiche: 3
+
     }
 }
